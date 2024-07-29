8a68dbb7b05fcf75e0dccde32f0b4699e39346b7
S <shakibmohammad18@gmail.com>
2024-07-29 23:37:22 +0600

    index check and data show in console

--- lib/controller/api_call/mobile_model_list.dart
+++ lib/controller/api_call/mobile_model_list.dart
@@ -7,8 +7,8 @@ static Future<dynamic> MobileList()async{
   String url =("https://api.restful-api.dev/objects");
   Uri uri = Uri.parse(url);
   var response = await http.get(uri);
-  var data = response.statusCode;
-  log("$data");
+  var data = jsonDecode(response.body);
+  log("${data[0]['data']['color']}");
   return data;
 
 
